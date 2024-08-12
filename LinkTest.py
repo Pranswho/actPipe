cadefc618c848b2c1a64455789c636fe507b1939
wickedseer <84715134+wickedseer@users.noreply.github.com>
2024-08-12 15:50:26 +0530

    Added Test Cases

---
+++ LinkTest.py
@@ -0,0 +1,20 @@
+from LinkedList import LinkedList
+
+def test_linked_list():
+    
+    ll = LinkedList()
+
+    ll.insertAtBeginning("Apple",10)
+    ll.insertAtBeginning("Orange",40)
+    ll.insertAtBeginning("Pineapple", 50)
+
+    assert ll.head.itemName == "Pineapple", "Test Case 1 Failed"
+    assert ll.head.next.itemName == "Orange", "Test Case 2 Failed"
+    assert ll.head.next.next.itemName == "Orange", "Test Case 3 Failed"
+
+    ll.traverse()
+    print("==================")
+
+
+if __name__ == "__main__":
+    test_linked_list()
